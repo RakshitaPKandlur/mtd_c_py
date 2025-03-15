2bb565781b05ca6a181ff1214474b845c78002d7
RakshitaPKandlur <rakshitakandlur7@gmail.com>
2025-03-15 12:48:39 +0530

    Delete day6 directory

--- day6/p1.py
+++
@@ -1,3 +0,0 @@
-n = int(input('Enter a number < 20:'))
-for i in range(1, n + 1):
-    print(' ' * (n - i) + '#' * i)
--- day6/p2.py
+++
@@ -1,7 +0,0 @@
-from prime import check_is_prime
-
-number =int(input('Check if a number is Prime: '))
-if check_is_prime(number):
-    print(f'{number} is a Prime number')
-else:
-    print(f'{number} is not a Prime number')
